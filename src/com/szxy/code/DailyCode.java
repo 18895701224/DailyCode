cdaf06c6e8e6780e7ca3934d8af769e8cfadb58c
ykc <1057227086@qq.com>
2019-12-17 21:04:55 +0800

    新增代码

--- src/com/szxy/code/DailyCode.java
+++ src/com/szxy/code/DailyCode.java
@@ -64,9 +64,33 @@ public class DailyCode {
 		for (double v : arr) {
 			System.out.println(v);
 		}
+	}
+	//选择排序
+	@Test
+	public void selectOrder(){
+		double[] arr = new double[10];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = Math.random()*100;
 
+		}
+		for (int i = 0; i < arr.length-1; i++) {
+			int minIndex = i;
+			for (int j = i+1; j <arr.length; j++) {
+				if(arr[j]<arr[minIndex]){
+					minIndex = j;
+				}
+			}
+			if(minIndex != i){
+				double tem = arr[i];
+				arr[i] = arr[minIndex];
+				arr[minIndex] = tem;
 
+			}
 
+		}
+		for (double v : arr) {
+			System.out.println(v);
+		}
 
 	}
 }
